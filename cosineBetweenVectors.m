2f379a0fbeefb1904f490a161e17573c01c33cea
monilpat <monilpat@gmail.com>
2019-03-06 14:06:54 -0800

    Painting Classifier

--- cosineBetweenVectors.m
+++
@@ -1,6 +0,0 @@
-function angle = cosineBetweenVectors (vector1, vector2) 
-    if (~isvector(vector1) || ~isvector(vector2))
-        error ('Input must be a vector')
-    end
-    angle = dot(vector1,vector2)/(norm(vector1) * norm(vector2));
-end
